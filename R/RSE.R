6cd29ecf8452f4e18270496ae83be8ef30cb1b97
MalteThodberg <maltethodberg@gmail.com>
2017-05-05 11:30:56 +0200

    Added RSE wrapper function

--- R/RSE.R
+++ R/RSE.R
@@ -1,38 +1,95 @@
-# #' Collect CAGE data into SummarizedExperiment
-# #'
-# #' Collects CTSS-GRanges, Tag Cluster (TCs), Expression Matrix (EM) and study design into a SummarizedExperiment. Additional options for documenting the object are available.
-# #'
-# #' @param ctss GRangesList: CTSS-GRanges.
-# #' @param tcs GRanges: TCs
-# #' @param em matrix, integer: EM
-# #' @param design data.frame: Study note
-# #'
-# #' @note This function does not sort or match the input, so arguments must be provided with matching columns, rows, etc.
-# #'
-# #' @return RangedSummarizedExperiment
-# #' @examples
-# #' # ADD_EXAMPLES_HERE
-# #' @export
-# summariseCAGE <- function(ctss, design, ctssCutoff, clusteringFun, ...){
-# 	### TO DO
-# 	# Option for keepnig ctss in final object
-# 	# Get column names from design rownames
-# 	# Option for time stamping object
-# 	# Option for author stamp
-#
-# 	tmp <- dim(em)
-# 	message("Assembling CAGE experiment of ", tmp[1], " TCs in ", tmp[2], " samples...")
-#
-# 	# Preserve CTSS files
-# 	sample_data <- S4Vectors::DataFrame(ctss=ctss, design)
-# 	rownames(sample_data) <- rownames(design)
-# 	colnames(em) <- rownames(design)
-#
-# 	# Assemble
-# 	RSE <- SummarizedExperiment::SummarizedExperiment(assays=list(counts=em),
-# 															rowRanges=tcs,
-# 															colData=sample_data)
-#
-# 	# Return
-# 	RSE
-# }
+#' Complete basic CAGE analysis
+#'
+#' This functions wraps a number of functions: readCTSS, coverageOfCTSS, tuneTagclusting, tagClustering & quantifyFeatures. Returns results as a RangedSummarizedExperiment
+#'
+#' @param bwPlus BigWigFileList: BigWig files with plus-strand CTSS data.
+#' @param bwMinus BigWigFileList: BigWig files with minus-strand CTSS data.
+#' @param design DataFrame or data.frame: Information about individual samples.
+#' @param genome Seqinfo or NULL: If not NULL, use the supplied Seqinfo as genome.
+#' @param keepCoverage logical: Whether to keep global coverage in the output object.
+#' @param mergeDist integer: Merge TCs within this distance.
+#' @param stepSize integer: Size of steps for increasing threshold.
+#' @param biocParallel BiocParallelParam: Settings for parallel backend.
+#'
+#' @return RangedSummarizedExperiment.
+#' @examples
+#' # ADD_EXAMPLES_HERE
+#' @import GenomicRanges SummarizedExperiment
+#' @export
+quickCAGE <- function(bwPlus, bwMinus, design, genome=NULL, keepCoverage=FALSE, mergeDist=20, stepSize=10, biocParallel=bpparam()){
+	### Pre-checks
+	# Classes
+	stopifnot(class(bwPlus) == "BigWigFileList",
+						class(bwMinus) == "BigWigFileList",
+						class(design) == "DataFrame" | is.data.frame(design))
+	stopifnot(identical(length(bwPlus), length(bwMinus), nrow(design)))
+
+	# Rownames to enable matching
+	stopifnot(!is.null(names(bwPlus)),
+						!is.null(names(bwMinus)),
+						!is.null(rownames(design)))
+
+	stopifnot(all(names(bwPlus) == names(bwMinus)))
+	stopifnot(all(rownames(design) == names(bwMinus)))
+
+	### Wrap functions
+
+	# Read in CTSS files
+	message("### Running readCTSS...")
+	ctss_grl <- readCTSS(bwPlus=bwPlus, bwMinus=bwMinus, genome = genome,
+											 outFormat = "GRangesList", biocParallel = biocParallel)
+
+	# Coverage
+	message("\n### Running coverageOfCTSS...")
+	global_cov <- coverageOfCTSS(grl=ctss_grl)
+
+	# Tune
+	message("\n### Running tuneTagClustering...")
+	bestTC <- tuneTagClustering(global_cov, mergeDist=mergeDist, stepSize=stepSize)
+	#optimalTC <- min(subset(bestTC, nTCs == max(nTCs), select=threshold))
+	optimalTC <- min(bestTC[bestTC$nTCs == max(bestTC$nTCs),])
+
+	# TCs
+	message("\n### Running tagClustering...")
+	TCs <- tagClustering(ctssCoverage=global_cov,
+											 tpmCutoff=optimalTC,
+											 mergeDist=mergeDist)
+
+	# Quantify TCs
+	message("\n### Running quantifyFeatures...")
+	CM <- quantifyFeatures(ctss=ctss_grl, features=TCs,
+												 biocParallel = biocParallel)
+
+	### Assemble RSE
+	message("\n### Assembling RangedSummarizedExperiment...")
+
+	# As summarized experiment
+	RSE <- SummarizedExperiment(assays=list(counts=CM),
+																	rowRanges=TCs,
+																	colData=design)
+
+	# Stamp with TPM cutoff
+	metadata(RSE)$optimalTPM <- optimalTC
+
+	# Stamp with metaData
+	metadata(RSE)$user <- Sys.getenv("USER")
+	metadata(RSE)$data <- date()
+	metadata(RSE)$sessionInfo <- utils::sessionInfo()
+
+
+	# Check if coverage should be kept in output
+	if(keepCoverage){
+		metadata(RSE)$globalCoverage <- global_cov
+		stopifnot(seqinfo(RSE) != seqinfo(global_cov))
+	}
+
+	### Post-checks
+
+	# Post-checks
+	stopifnot(class(RSE) == "RangedSummarizedExperiment",
+						!is.null(rownames(RSE)),
+						!is.null(colnames(RSE)))
+
+	# Return
+	RSE
+}
